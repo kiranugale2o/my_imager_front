f89e05bb4ecb319f2ffcc95847e4b751b5c5dda7
kiran <kiranugale239@gmail.com>
2025-01-20 23:18:03 +0530

    add dashboardLayout

---
+++ src/components/ui/dashboard_layout/index.js
@@ -0,0 +1,47 @@
+"use client";
+import { useState } from "react";
+import DashboardLeftNavbar from "../headers/dashboardLeftbar";
+import DashboardTopbar from "../headers/dashboardTopbar.js";
+import LeftNavbar from "../headers/leftbar";
+import ProjectPageLayout from "./projectpage_layout";
+
+export default function DashboardLayout() {
+  const [Mypage, setPage] = useState("project");
+
+  return (
+    <>
+      <div className="bg-black text-white flex flex-col">
+        <div className="flex w-full">
+          <DashboardTopbar />
+        </div>
+        <div className="flex w-full">
+          <div className="flex w-auto lg:w-[250px]">
+            <DashboardLeftNavbar setPage={setPage} />
+          </div>
+          <div className="flex w-full p-5 lg:w-[900px]">
+            {Mypage === "project" ? (
+              <div className="flex">
+                <ProjectPageLayout />
+              </div>
+            ) : null}
+            {Mypage === "Organizations" ? (
+              <div className="flex">Organizations page</div>
+            ) : null}
+            {Mypage === "Account" ? (
+              <div className="flex">Account page</div>
+            ) : null}
+            {Mypage === "Preferences" ? (
+              <div className="flex">Preferences page</div>
+            ) : null}
+            {Mypage === "Security" ? (
+              <div className="flex">Security page</div>
+            ) : null}
+            {Mypage === "Access_Tokens" ? (
+              <div className="flex">Access_Token page</div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/ui/dashboard_layout/projectpage_layout/index.js
@@ -0,0 +1,52 @@
+import { ArrowRightFromLineIcon } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../../card";
+
+export default function ProjectPageLayout() {
+  const maps = [1, 2, 2, 3, 3, 3];
+  return (
+    <>
+      <div className="flex lg:p-5 lg:px-10 flex-col w-full gap-5">
+        <div className="flex">
+          <button className="p-1 rounded-md px-2 hover:border hover:border-red-500 bg-gradient-to-r from-red-300 via-red-500 to-purple-700 font-normal  text-sm">
+            New Project
+          </button>
+        </div>
+        <div className="text-lg text-white font-semibold font-mono antialiased">
+          Dear Kiran Your Projects
+        </div>
+
+        <div className="block lg:flex lg:flex-wrap w-full  gap-3 grid  gap-y-6 lg:gap-3 ">
+          {maps.map((d, i) => {
+            return (
+              <Card
+                key={i}
+                className=" w-[330px] lg:w-[250px] bg-slate-300 text-black border-gray-200 hover:bg-slate-50"
+              >
+                <CardHeader className="">
+                  <CardTitle className="flex justify-between">
+                    <span className="text-sm">Project Name</span>
+                    <ArrowRightFromLineIcon className="hover:size-7" />
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-md">size:45GB</div>
+                </CardContent>
+                <CardFooter>
+                  <div className="text-sm font-normal ">
+                    Created At.12 jan 2025
+                  </div>
+                </CardFooter>
+              </Card>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
