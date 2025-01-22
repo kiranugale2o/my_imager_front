a8915509eb66c27c368a8529739589b24ad293b6
kiran <kiranugale239@gmail.com>
2025-01-22 23:32:02 +0530

    add project Details page

---
+++ src/app/dashboard/[projectId]/page.js
@@ -0,0 +1,9 @@
+import ProjectDetailsPage from "@/components/ui/dashboard_layout/projectpage_layout/project_details";
+
+export default function ProjectPage() {
+  return (
+    <>
+      <ProjectDetailsPage></ProjectDetailsPage>
+    </>
+  );
+}
