f72e337fe7fb8f92a8ad5cb7bfcc443754e54cd0
kiran <kiranugale239@gmail.com>
2025-01-24 22:54:13 +0530

    add security page

---
+++ src/components/ui/dashboard_layout/security_page/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const SettingsPage = () => {
+  return (
+    <div className="flex flex-col w-full ">
+      <div className="text-3xl  p-6 border-b border-gray-500  font-sans font-extralight">
+        Security
+      </div>
+
+      <div className="mt-5 border border-gray-700 rounded-lg p-5 ">
+        We Provide Two Authenticate Security to Our Users.
+      </div>
+    </div>
+  );
+};
+
+export default SettingsPage;
