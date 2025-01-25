a7bab1f1c922c78022742509442cddfbbef4db73
kiran <kiranugale239@gmail.com>
2025-01-25 23:22:52 +0530

    add blog page

---
+++ src/app/blog/[blogId]/page.js
@@ -0,0 +1,9 @@
+import BlogDetailPage from "@/components/ui/blog_layout/blog_details";
+
+export default function BlogDetails() {
+  return (
+    <>
+      <BlogDetailPage />
+    </>
+  );
+}
---
+++ src/app/blog/page.js
@@ -0,0 +1,9 @@
+import BlogPage from "@/components/ui/blog_layout";
+
+export default function Blog() {
+  return (
+    <div className="lg:p-24">
+      <BlogPage />
+    </div>
+  );
+}
