72acf009fd29a18ec38426b2c92c5ba413e99476
kiran <kiranugale239@gmail.com>
2025-01-28 23:12:31 +0530

    add 404 page

---
+++ src/app/not-found.js
@@ -0,0 +1,21 @@
+// pages/404.js
+
+export default function Custom404() {
+  return (
+    <div className="h-screen bg-black flex justify-center items-center text-center text-white">
+      <div className="bg-dark-gray p-8 rounded-lg shadow-xl">
+        <h1 className="text-5xl font-bold text-red-500 mb-4">404</h1>
+        <h2 className="text-3xl font-semibold mb-6">Oops! Page Not Found</h2>
+        <p className="text-lg text-gray-300 mb-6">
+          Sorry, the page you’re looking for doesn’t exist.
+        </p>
+        <a
+          href="/"
+          className="bg-red-500 text-white px-6 py-3 rounded-md hover:bg-red-600 transition-colors"
+        >
+          Go Back to Home
+        </a>
+      </div>
+    </div>
+  );
+}
