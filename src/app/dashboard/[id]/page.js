eb29425ddc2917e15c0fcdc7b5dec413061c08cb
kiran <kiranugale239@gmail.com>
2025-01-28 23:14:11 +0530

    add page details page

---
+++ src/app/dashboard/[id]/page.js
@@ -0,0 +1,40 @@
+import { getProjectDetails, getUserDetails } from "@/actions";
+
+import ProjectDetailsPage from "@/components/ui/dashboard_layout/projectpage_layout/project_details";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function ProjectPage({ params }) {
+  const { isAuthenticated } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) {
+    redirect("/api/auth/login");
+  }
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const userExit = await getUserDetails(user?.id);
+
+  const { id } = await params;
+  const details = await getProjectDetails(id);
+
+  if (userExit) {
+    return (
+      <>
+        {!details ? (
+          <ProjectDetailsPage
+            user={userExit}
+            Projectdetails={details}
+          ></ProjectDetailsPage>
+        ) : (
+          <>
+            <div className="text-center text-3xl text-white">
+              Project not found !
+            </div>
+          </>
+        )}
+      </>
+    );
+  } else {
+    redirect("/onboard");
+  }
+}
